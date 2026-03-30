7609e17b3747f4a7139417351a918299f90b8317
Juan Camilo Quevedo Jaimes <juanqueja64@gmail.com>
2026-03-29 23:31:25 -0500

    Finalizando requerimientos

---
+++ app.js
@@ -0,0 +1,118 @@
+//! Mapeamos los 3 numeros ingresados:
+const mostrarUno = document.getElementById("num1")
+const mostrarDos = document.getElementById("num2")
+const mostrarTres = document.getElementById("num3")
+
+//! Mapeamos los campos para mostrar en el HTML (7)
+const mostrarMayor = document.querySelector(".numMayor")
+const mostrarCentro = document.querySelector(".numCentro")
+const mostrarMenor = document.querySelector(".numMenor")
+
+const mostrarMayor1 = document.querySelector(".numMayor1")
+const mostrarCentro1 = document.querySelector(".numCentro1")
+const mostrarMenor1 = document.querySelector(".numMenor1")
+
+const mostrarIguales = document.getElementById("sonIguales")
+
+
+
+//! Pedimos los numeros mediante PROMPT.
+let num1 = prompt("Ingrese el primer valor")
+let num2 = prompt("Ingrese el segundo valor")
+let num3 = prompt("Ingrese el tercer valor")
+
+// let num1 = 9
+// let num2 = 4
+// let num3 = 9
+
+mostrarUno.textContent = num1
+mostrarDos.textContent = num2
+mostrarTres.textContent = num3
+
+
+if (num1 == num2 && num1 == num3) {
+    console.log(`Los numeros son iguales`);
+    mostrarIguales.textContent = "Los numeros son iguales"
+    mostrarIguales.style.color = "green"
+    mostrarMayor.textContent = num1
+    mostrarCentro.textContent = num2
+    mostrarMenor.textContent = num3
+    mostrarMayor1.textContent = num1
+    mostrarCentro1.textContent = num2
+    mostrarMenor1.textContent = num3
+}
+
+else if (num1 >= num2 && num1 >= num3) {
+    if (num2 >= num3) {
+        console.log(`${num1} es el numero mayor`);
+        console.log(`${num2} es el numero centro`);
+        console.log(`${num3} es el numero menor`);
+        mostrarMayor.textContent = num1
+        mostrarCentro.textContent = num2
+        mostrarMenor.textContent = num3
+        mostrarMayor1.textContent = num1
+        mostrarCentro1.textContent = num2
+        mostrarMenor1.textContent = num3
+    }
+    else if (num3 >= num2) {
+        console.log(`${num1} es el numero mayor`);
+        console.log(`${num3} es el numero centro`);
+        console.log(`${num2} es el numero menor`);
+        mostrarMayor.textContent = num1
+        mostrarCentro.textContent = num3
+        mostrarMenor.textContent = num2
+        mostrarMayor1.textContent = num1
+        mostrarCentro1.textContent = num3
+        mostrarMenor1.textContent = num2
+    }
+}
+
+else if (num2 >= num3 && num2 >= num1) {
+    if (num3 >= num1) {
+        console.log(`${num2} es el numero mayor`);
+        console.log(`${num3} es el numero centro`);
+        console.log(`${num1} es el numero menor`);
+        mostrarMayor.textContent = num2
+        mostrarCentro.textContent = num3
+        mostrarMenor.textContent = num1
+        mostrarMayor1.textContent = num2
+        mostrarCentro1.textContent = num3
+        mostrarMenor1.textContent = num1
+    }
+    else if (num1 >= num3) {
+        console.log(`${num2} es el numero mayor`);
+        console.log(`${num1} es el numero centro`);
+        console.log(`${num3} es el numero menor`);
+        mostrarMayor.textContent = num2
+        mostrarCentro.textContent = num1
+        mostrarMenor.textContent = num3
+        mostrarMayor1.textContent = num2
+        mostrarCentro1.textContent = num1
+        mostrarMenor1.textContent = num3
+    }
+}
+
+else if (num3 >= num2 && num3 >= num1) {
+    if (num2 >= num1) {
+        console.log(`${num3} es el numero mayor`);
+        console.log(`${num2} es el numero centro`);
+        console.log(`${num1} es el numero menor`);
+        mostrarMayor.textContent = num3
+        mostrarCentro.textContent = num2
+        mostrarMenor.textContent = num1
+        mostrarMayor1.textContent = num3
+        mostrarCentro1.textContent = num2
+        mostrarMenor1.textContent = num1
+    }
+    else if (num1 >= num2) {
+        console.log(`${num3} es el numero mayor`);
+        console.log(`${num1} es el numero centro`);
+        console.log(`${num2} es el numero menor`);
+        mostrarMayor.textContent = num3
+        mostrarCentro.textContent = num1
+        mostrarMenor.textContent = num2
+        mostrarMayor1.textContent = num3
+        mostrarCentro1.textContent = num1
+        mostrarMenor1.textContent = num2
+    }
+}
